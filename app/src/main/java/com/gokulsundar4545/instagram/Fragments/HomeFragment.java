dbc1cc1713fcd0b2fd125f8d107dfb0853e5ce21
GokulSundarajv <gokulpoongodi4545s@gmail.com>
2022-07-05 14:01:13 +0530

    final

---
+++ app/src/main/java/com/gokulsundar4545/instagram/Fragments/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.gokulsundar4545.instagram.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gokulsundar4545.instagram.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/com/gokulsundar4545/instagram/PostActivity.java
@@ -0,0 +1,14 @@
+package com.gokulsundar4545.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
